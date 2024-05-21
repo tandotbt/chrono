fe7e443eb98451dfddbbd3b580a181e99866d14d
Moreal <dev.moreal@gmail.com>
2024-05-21 13:35:05 +0900

    refactor: separate confirmation logic

--- background/src/wallet/wallet.ts
+++ background/src/wallet/wallet.ts
@@ -32,12 +32,7 @@ import { Emitter } from "../event";
 import { Buffer } from "buffer";
 import { PopupController } from "@/controllers/popup";
 import { NetworkController } from "@/controllers/network";
-
-interface Request {
-	id: string;
-	category: string;
-	data: unknown;
-}
+import { ConfirmationController } from "@/controllers/confirmation";
 
 interface SavedTransactionHistory {
 	id: string;
@@ -54,13 +49,12 @@ interface SavedTransactionHistory {
 	};
 }
 
-const pendingApprovals = new Map();
-
 export default class Wallet {
 	private readonly storage: Storage;
 	private readonly api: Graphql;
 	private readonly popup: PopupController;
 	private readonly networkController: NetworkController;
+	private readonly confirmationController: ConfirmationController;
 	private readonly passphrase: Lazyable<string>;
 	private readonly emitter: Emitter;
 	private readonly origin: string | undefined;
@@ -79,11 +73,13 @@ export default class Wallet {
 		api: Graphql,
 		popupController: PopupController,
 		networkController: NetworkController,
+		confirmationController: ConfirmationController,
 		emitter: Emitter | undefined,
 	) {
 		this.storage = storage;
 		this.api = api;
 		this.popup = popupController;
+		this.confirmationController = confirmationController;
 		this.passphrase = passphrase;
 		this.emitter = emitter;
 		this.origin = origin;
@@ -114,6 +110,10 @@ export default class Wallet {
 		const storage = new Storage(passphrase);
 		const api = await Graphql.createInstance(storage);
 		const networkController = new NetworkController(storage, emitter);
+		const approvalRequestController = new ConfirmationController(
+			storage,
+			popup,
+		);
 		return new Wallet(
 			passphrase,
 			origin,
@@ -121,6 +121,7 @@ export default class Wallet {
 			api,
 			popup,
 			networkController,
+			approvalRequestController,
 			emitter,
 		);
 	}
@@ -253,47 +254,52 @@ export default class Wallet {
 			throw new Error("Invalid action. action must be BencodexDictionary.");
 		}
 
-		return this._requestApprove("sign", {
-			signer,
-			content: convertBencodexToJSONableType(action),
-		}).then(async () => {
-			const wallet = await this.loadWallet(signer, resolve(this.passphrase));
-			const account = RawPrivateKey.fromHex(wallet.privateKey.slice(2));
-			const sender = Address.fromHex(wallet.address);
-			const currentNetwork = await this.networkController.getCurrentNetwork();
-			const genesisHash = Buffer.from(currentNetwork.genesisHash, "hex");
-
-			const actionTypeId = action.get("type_id");
-			const gasLimit =
-				typeof actionTypeId === "string" &&
-				actionTypeId.startsWith("transfer_asset")
-					? BigInt(4)
-					: BigInt(1);
-
-			const unsignedTx = {
-				signer: sender.toBytes(),
-				actions: [action],
-				updatedAddresses: new Set([]),
-				nonce: BigInt(await this.nextNonce(sender.toString())),
-				genesisHash,
-				publicKey: (await account.getPublicKey()).toBytes("uncompressed"),
-				timestamp: new Date(),
-				maxGasPrice: {
-					currency: {
-						ticker: "Mead",
-						decimalPlaces: 18,
-						minters: null,
-						totalSupplyTrackable: false,
-						maximumSupply: null,
-					},
-					rawValue: BigInt(Decimal.pow(10, 18).toString()),
+		return this.confirmationController
+			.request({
+				category: "sign",
+				data: {
+					signer,
+					content: convertBencodexToJSONableType(action),
 				},
-				gasLimit,
-			};
+			})
+			.then(async () => {
+				const wallet = await this.loadWallet(signer, resolve(this.passphrase));
+				const account = RawPrivateKey.fromHex(wallet.privateKey.slice(2));
+				const sender = Address.fromHex(wallet.address);
+				const currentNetwork = await this.networkController.getCurrentNetwork();
+				const genesisHash = Buffer.from(currentNetwork.genesisHash, "hex");
 
-			const signedTx = await this._signTx(signer, unsignedTx);
-			return Buffer.from(encode(encodeSignedTx(signedTx))).toString("hex");
-		});
+				const actionTypeId = action.get("type_id");
+				const gasLimit =
+					typeof actionTypeId === "string" &&
+					actionTypeId.startsWith("transfer_asset")
+						? BigInt(4)
+						: BigInt(1);
+
+				const unsignedTx = {
+					signer: sender.toBytes(),
+					actions: [action],
+					updatedAddresses: new Set([]),
+					nonce: BigInt(await this.nextNonce(sender.toString())),
+					genesisHash,
+					publicKey: (await account.getPublicKey()).toBytes("uncompressed"),
+					timestamp: new Date(),
+					maxGasPrice: {
+						currency: {
+							ticker: "Mead",
+							decimalPlaces: 18,
+							minters: null,
+							totalSupplyTrackable: false,
+							maximumSupply: null,
+						},
+						rawValue: BigInt(Decimal.pow(10, 18).toString()),
+					},
+					gasLimit,
+				};
+
+				const signedTx = await this._signTx(signer, unsignedTx);
+				return Buffer.from(encode(encodeSignedTx(signedTx))).toString("hex");
+			});
 	}
 
 	async signTx(signer: string, encodedUnsignedTxHex: string): Promise<string> {
@@ -341,13 +347,16 @@ export default class Wallet {
 	}
 
 	async connect(): Promise<void> {
-		return this._requestApprove("connect", { origin: this.origin }).then(
-			async (metadata) => {
+		return this.confirmationController
+			.request({
+				category: "connect",
+				data: { origin: this.origin },
+			})
+			.then(async (metadata) => {
 				const connectedSites = await this._getConnectedSites();
 				connectedSites[this.origin] = metadata;
 				await this._setConnectedSites(connectedSites);
-			},
-		);
+			});
 	}
 
 	async isConnected(): Promise<boolean> {
@@ -362,73 +371,6 @@ export default class Wallet {
 	async _setConnectedSites(sites) {
 		console.log("sites", sites);
 		await this.storage.set(CONNECTED_SITES, sites);
-	}
-
-	async _requestApprove(category: string, data: unknown) {
-		const requestId = nanoid();
-		await this.addRequest({
-			id: requestId,
-			category,
-			data,
-		});
-
-		await this.popup.show();
-
-		return new Promise((resolve, reject) => {
-			pendingApprovals.set(requestId, { resolve, reject });
-		});
-	}
-
-	async hasApprovalRequest(): Promise<boolean> {
-		const requests = await this.getApprovalRequests();
-		return requests.length > 0;
-	}
-
-	async addRequest(request: Request): Promise<void> {
-		const requests = await this.getApprovalRequests();
-		if (requests.find(({ id }) => id === request.id)) {
-			throw new Error("Duplicated request.");
-		}
-
-		await this.setApprovalRequests([...requests, request]);
-	}
-
-	async approveRequest(requestId: string, metadata) {
-		const requests = await this.getApprovalRequests();
-		await this.setApprovalRequests(
-			requests.filter(({ id }) => id !== requestId),
-		);
-
-		const handlers = pendingApprovals.get(requestId);
-		console.log(requestId, pendingApprovals);
-		if (handlers !== null) {
-			handlers.resolve(metadata);
-		}
-	}
-
-	async rejectRequest(requestId: string) {
-		const requests = await this.getApprovalRequests();
-		await this.setApprovalRequests(
-			requests.filter(({ id }) => id !== requestId),
-		);
-
-		const handlers = pendingApprovals.get(requestId);
-		console.log(requestId, pendingApprovals);
-		if (handlers !== null) {
-			handlers.reject();
-		}
-	}
-
-	/**
-	 * @returns {Promise<Array>}
-	 */
-	async getApprovalRequests(): Promise<Request[]> {
-		const requests = JSON.parse(await this.storage.rawGet(APPROVAL_REQUESTS));
-		if (requests === null) {
-			return [];
-		}
-
-		return requests;
 	}
 
 	async listAccounts(): Promise<Account[]> {
@@ -449,11 +391,6 @@ export default class Wallet {
 	async getPublicKey(address: string): Promise<string> {
 		const wallet = await this.loadWallet(address, resolve(this.passphrase));
 		return wallet.publicKey;
-	}
-
-	async setApprovalRequests(requests: Request[]): Promise<void> {
-		console.log("setApprovalRequests", requests);
-		await this.storage.rawSet(APPROVAL_REQUESTS, JSON.stringify(requests));
 	}
 }
 
