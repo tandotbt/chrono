8d09922c5b68e2826425bd33349ce145f1041d58
tanbt <92938902+tandotbt@users.noreply.github.com>
2024-06-02 05:58:02 +0000

    Change to use for nineCMD

--- packages/chrono-sdk/src/chrono-wallet.ts
+++ packages/chrono-sdk/src/chrono-wallet.ts
@@ -53,12 +53,11 @@ export class ChronoWallet {
         });
     }
 
-    sign(signer: Address, action: PolymorphicAction): Promise<Buffer> {
+    sign(signer: Address, action: string): Promise<Buffer> {
         return new Promise((resolve, reject) => {
             this.handler.send(
                 { resolve: (value: string) => resolve(Buffer.from(value, "hex")), reject },
-                { method: 'sign', signer: signer.toString(), action: Buffer.from(action.serialize()).toString("hex") }
-            );
+                { method: 'sign', signer: signer.toString(), action: action }); // No need Buffer
         });
     }
 
@@ -78,10 +77,10 @@ export class ChronoWallet {
             this.handler.send(
                 {
                     resolve: (value: {
-                        address: string,
+                        address: Address,
                     }[]) => resolve(value.map(x => {
                         return {
-                            address: Address.fromHex(x.address)
+                            address: x.address
                         };
                     })),
                     reject
@@ -94,7 +93,7 @@ export class ChronoWallet {
         return new Promise((resolve, reject) => {
             this.handler.send(
                 {
-                    resolve: (value: string) => resolve(PublicKey.fromHex(value, "uncompressed")),
+                    resolve: (value: string) => resolve(PublicKey.fromHex(value.slice(2), "uncompressed")), // Remove '0x' from value
                     reject
                 },
                 { method: "getPublicKey", address: address.toString(), }
